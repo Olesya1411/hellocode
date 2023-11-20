d830eebd6c1921ff285334c8f94db5fdca81a7be
Olesya1411 <blinova.olesya@inbox.ru>
2023-11-20 21:59:16 +0400

    add file ifelse

---
+++ examples_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha") // операция tolower переводит все символы строки в нижний регистр
+{
+    Console.WriteLine("Yess! This is Masha!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
